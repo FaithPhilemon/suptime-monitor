0484c03d3a27c704f032e8e700bdcf2ba495f1bd
Faith Philemon <philemonfaith@gmail.com>
2019-09-12 00:07:00 +0100

    parse requests paths

--- index.js
+++ index.js
@@ -4,13 +4,27 @@
 
 //  Dependencies
 var http = require('http');
+var url = require('url');
 
 // Server should respond to all requests with a string
 var server = http.createServer(function (req, res) {
+    
+    //get the URL and parse it.
+    var parsedUrl = url.parse(req.url, true);
+
+    // get the path.
+    var path = parsedUrl.pathname;
+    var trimedpath = path.replace(/^\/+|\/+$/g, '');
+
+    // send response.
     res.end("Hello World\n");
+
+    // log requests
+    console.log('Request recieved on path: ' + trimedpath);
+    
 });
 
 // Start the server and have it listen to port 3000
-server.listen(3000, function () {
-    console.log('Server listening on port 3000 now!');
+server.listen(8000, function () {
+    console.log('Server listening on port 8000 now!\n');
 });
