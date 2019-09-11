1167bb0600646f82128dc0441f10137547c64548
Faith Philemon <philemonfaith@gmail.com>
2019-09-11 23:23:35 +0100

    aplha commit & server config

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Primary file for API
+ */
+
+//  Dependencies
+var http = require('http');
+
+// Server should respond to all requests with a string
+var server = http.createServer(function (req, res) {
+    res.end("Hello World\n");
+});
+
+// Start the server and have it listen to port 3000
+server.listen(3000, function () {
+    console.log('Server listening on port 3000 now!');
+});
